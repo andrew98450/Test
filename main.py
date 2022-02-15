bd095ad7827e849617d277ae2cedd2f1d49a3ce3
Andrew Huang <ktpss96034@gmail.com>
2022-02-15 19:26:54 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-import os
+import remi
+import remi.gui as gui
 
-from flask import Flask
+class ChatApp(remi.App):
 
-app = Flask(__name__)
+    def __init__(self, request, client_address, server, **app_args):
+        super().__init__(request, client_address, server, **app_args)
+     
+    def main(self):
+        container = gui.VBox(width=500, height=200)
+        self.label = gui.Label("Hello")
+        self.textbox = gui.TextInput()
+        self.bt = gui.Button("Click")
+        self.bt.onclick.do(self.on_button_click)
 
+        container.append(self.label)
+        container.append(self.textbox)
+        container.append(self.bt)
+        return container
 
-@app.route("/")
-def hello_world():
-    name = os.environ.get("NAME", "World")
-    return "Hello {}!".format(name)
+    def on_button_click(self, w):
+        self.label.set_text(self.textbox.get_text())
 
+    def on_close(self):
+        return super().on_close()
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 80)))
+remi.start(ChatApp, port=8080)
